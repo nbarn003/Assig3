874c73c7705b3ae05b25dba8cda099c969684176
gjohn009 <73452246+gjohn009@users.noreply.github.com>
2020-10-25 22:15:58 -0400

    Update mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -34,15 +34,15 @@ private slots:
 
     void on_but_overlay_clicked();
 
-    void on_btn_load_clicked();
+    void on_btn_load_clicked(); // To load
 
-    void on_but_save_clicked();
+    void on_but_save_clicked(); // To save
 
-    void on_sldr_brightness_valueChanged(int value);
+    void on_sldr_brightness_valueChanged(int value); // Slider for brightness
 
-    void on_sldr_contrast_valueChanged(int value);
+    void on_sldr_contrast_valueChanged(int value); // Slider for constrast
 
-    void on_pushButton_clicked();
+    void on_pushButton_clicked(); // to send address
 
 private:
     Ui::MainWindow *ui;
@@ -52,7 +52,7 @@ private:
     cv::Mat brconImage;
     int contrast_value;
     int brightness_value;
-    QImage brconOut;
-    QUdpSocket *socket;
+    QImage brconOut; 
+    QUdpSocket *socket; //to establish connection with IP address
 };
 #endif // MAINWINDOW_H
